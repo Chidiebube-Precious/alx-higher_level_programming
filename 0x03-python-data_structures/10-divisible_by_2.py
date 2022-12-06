61cb57759147d43947b13825f5051ba92df66df4
Chidiebube-Precious <precious.ezeokoye.235952@unn.edu.ng>
2022-12-06 12:37:54 -0800

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
-    for number in my_list:
-        if (number % 2) == 0:
-            new_list.append(True)
+def divisible_by_2(my_list):
+    new_list = my_list[:]
+    index = 0
+    for i in new_list:
+        if i % 2 == 0:
+            new_list[index] = True
         else:
-            new_list.append(False)
-        return(new_list)
+            new_list[index] = False
+        index += 1
+    return (new_list)
