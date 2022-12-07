cdcbffabb5f92da4b6df20aef769f097b06bf8a1
Chidiebube-Precious <precious.ezeokoye.235952@unn.edu.ng>
2022-12-07 07:12:37 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[y ** 2 for y in x] for x in matrix]
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
