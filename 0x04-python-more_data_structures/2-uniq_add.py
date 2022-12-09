10fcadb6324148e0ad5e797d9a0835cf53d015a9
Chidiebube-Precious <precious.ezeokoye.235952@unn.edu.ng>
2022-12-09 05:29:42 -0800

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    res_list = []
-    res = 0
-    for item in my_list:
-        if item not in res_list:
-            res_list.append(item)
-        for uniqs in res_list:
-            res += uniqs
-        return res
+    check_list = []
+    sum = 0
+    for i in my_list:
+        if not check_list:
+            sum += i
+            check_list.append(i)
+        else:
+            for j in check_list:
+                if j == i:
+                    checked = 1
+            if checked != 1:
+                sum += i
+                check_list.append(i)
+            return sum
+
